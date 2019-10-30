12ebbeacbf90345f5d0f83c5ea4b07df96c2eba0
David Rosenblum <david.rosenblum@neo4j.com>
2019-10-30 09:34:09 -0400

    added conversation id and sent on date
    added conversation id and sent on date

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace OutlookToNeo4j
            
 
             //New Bolt Graph Client
-            var graphClient = new BoltGraphClient("bolt://localhost:7687/", "neo4j", "neo");
+            var graphClient = new BoltGraphClient("bolt://localhost:7687/", "neo4j", "Myneo4j");
             graphClient.Connect();
 
             //Store the data from the DataProvider
@@ -36,17 +36,16 @@ namespace OutlookToNeo4j
                 if (data.From.Email != null)
                 { 
                     client.Cypher
-                        .Merge($"(from:{Person.Labels} {{Email: $frm.{nameof(Person.Email)} }})")
-                        .OnCreate().Set($"from.Name = $frm.{nameof(Person.Name)}")
-                        .Merge($"(e:{Email.Labels} {{Uuid: $data.{nameof(FromExchange.Id)}}})")
-                        .OnCreate().Set($"e.Subject = $data.{nameof(FromExchange.Subject)}")
+                        .Merge($"(from:{Person.Labels} {{email: $frm.{nameof(Person.Email)} }})")
+                        .OnCreate().Set($"from.name = $frm.{nameof(Person.Name)}")
+                        .Merge($"(e:{Email.Labels} {{id: $data.{nameof(FromExchange.Id)}}})")
+                        .OnCreate().Set($"e.subject = $data.{nameof(FromExchange.Subject)},e.conversation = $data.{nameof(FromExchange.Conversation)}, e.sentOn = $data.{nameof(FromExchange.SentOn)}")
                         .Merge($"(from)-[:{RelationshipTypes.Sent}]->(e)")
                         .With("e" )
                         .Unwind(data.To, "to")
-                        //.With("e,split($data.To,';') as vTo")
-                        //.Unwind("vTo" , "to")
-                        .Merge($"(t:{Person.Labels} {{Email: coalesce(to.{nameof(Person.Email)},'none')}})")
-                        .OnCreate().Set($"t.Name = to.{nameof(Person.Name)}")
+                        
+                        .Merge($"(t:{Person.Labels} {{email: coalesce(to.{nameof(Person.Email)},'none')}})")
+                        .OnCreate().Set($"t.name = to.{nameof(Person.Name)}")
                         .Merge($"(e)-[:{RelationshipTypes.Received}]->(t)")
                         .WithParams(new { data, frm = data.From })
                         .ExecuteWithoutResults();
@@ -90,7 +89,7 @@ namespace OutlookToNeo4j
                 if (ft == fi) ft = _random.Next(count);
                 var from = new Person { Email = $"user{fi}@testplace.com", Name = $"Test Person_{fi}" };
                 var to = new Person { Email = $"user{ft}@testplace.com", Name = $"Test Person_{ft}" };
-                _data.Add(new FromExchange { From = from, To = new List<Person> { to }, Id =  Guid.NewGuid().ToString(), Subject = $"Subject {_random.Next(count)}" });
+                _data.Add(new FromExchange { From = from, To = new List<Person> { to }, Id =  Guid.NewGuid().ToString(), Subject = $"subject {_random.Next(count)}" });
             }
         }
 
@@ -131,20 +130,22 @@ namespace OutlookToNeo4j
                     var stringBuilder = new StringBuilder();
                     stringBuilder.AppendLine($"From: {item.SenderEmailAddress}");
                     stringBuilder.AppendLine($"To: {item.To}");
-                    stringBuilder.AppendLine($"CC: {item.CC}");
-                    stringBuilder.AppendLine($"BCC: {item.BCC}");
+                    stringBuilder.AppendLine($"Conversation: {item.ConversationID}");
+                    //stringBuilder.AppendLine($"CC: {item.CC}");
+                    //stringBuilder.AppendLine($"BCC: {item.BCC}");
                     stringBuilder.AppendLine("");
                     stringBuilder.AppendLine($"Subject: {item.Subject}");
                     
                     Console.WriteLine(stringBuilder);
+                    var email = new Email { SentOn = item.SentOn };
                     var from = new Person {Email = item.SenderEmailAddress, Name = item.SenderName};
                     var cc = GetPersonsFromEmailString(item.CC);
                     var to = GetPersonsFromEmailString(item.To);
                     var bcc = GetPersonsFromEmailString(item.BCC);
 
-                    _data.Add(new FromExchange {From = @from, To = to, CC = cc, BCC = bcc, Id = item.EntryID, Subject = item.Subject});
+                    _data.Add(new FromExchange {From = @from, To = to, CC = cc, BCC = bcc, Id = item.EntryID, Conversation = item.ConversationID, Subject = item.Subject, SentOn=item.SentOn});
                     ii++;
-                    if (ii > 300) break;
+                    if (ii > 1000) break;
                     //Marshal.ReleaseComObject(item);
                 }
             }
@@ -190,9 +191,11 @@ namespace OutlookToNeo4j
         public IEnumerable<Person> To { get; set; }
         public IEnumerable<Person> CC { get; set; }
         public IEnumerable<Person> BCC { get; set; }
+        public string Conversation { get; set; }
 
         public string Subject { get; set; }
         public string Id { get; set; }
+        public DateTime SentOn { get; set; }
     }
 
     #region What we're storing
@@ -200,8 +203,9 @@ namespace OutlookToNeo4j
     {
         public const string Labels = "Email";
         public string Id { get; set; }
+        public string Conversation { get; set; }
         public string Subject { get; set; }
-
+        public DateTime SentOn { get; set; }
     }
 
     public class Person
